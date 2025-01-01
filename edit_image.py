e0754dc94cb17100b064c5956f810a3851ede42f
taKana671 <kanae5321@gmail.com>
2025-01-01 17:59:13 +0900

    Changed to be able to transparent image.

--- edit_image.py
+++ edit_image.py
@@ -1,12 +1,47 @@
 import pathlib
 import tkinter as tk
 import tkinter.ttk as ttk
-import tkinter.filedialog
+# import tkinter.filedialog
+from tkinter import messagebox, filedialog
+from typing import NamedTuple
+
 
 import cv2
 import numpy as np
 from PIL import Image, ImageTk
 
+
+class Size(NamedTuple):
+
+    rows: int
+    cols: int
+    color: int = None
+
+    def __len__(self):
+        return len([v for v in self if v is not None])
+
+    def scale(self, scale):
+        return int(self.rows * scale), int(self.cols * scale)
+
+    def is_inside(self, pt):
+        if pt.x <= self.cols and pt.y <= self.rows:
+            return True
+
+    def replace_outside_pt(self, pt):
+        x = self.cols if pt.x > self.cols else pt.x
+        y = self.rows if pt.y > self.rows else pt.y
+        return x, y
+
+
+class Point(NamedTuple):
+
+    x: int
+    y: int
+
+    def get_original_pt(self, scale):
+        org_x = int(self.x / scale)
+        org_y = int(self.y / scale)
+        return Point(org_x, org_y)
 
 
 class Window(ttk.Frame):
@@ -16,10 +51,11 @@ class Window(ttk.Frame):
         self.master.geometry('600x400')
         self.pack(fill=tk.BOTH, expand=True)
         self.master.bind('<Escape>', lambda event: self.master.destroy())
-        self.create_ui()
 
         self.start_pt = None
+        self.default_alpha = 50
 
+        self.create_ui()
         # img_bgr = cv2.imread('test_images/top_ground_src2.png')
         # img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
         # img_pil = Image.fromarray(img_rgb)
@@ -45,6 +81,7 @@ class Window(ttk.Frame):
         self.canvas.pack(fill=tk.BOTH, expand=True)
 
         self.canvas.bind('<Button-1>', self.click)
+        self.canvas.bind('<Button1-Motion>', self.mouse_drag)
         self.canvas.bind('<ButtonRelease-1>', self.release)
 
     def create_widget_area(self):
@@ -62,9 +99,13 @@ class Window(ttk.Frame):
         label = ttk.Label(frame, text='alpha')
         label.pack(side=tk.LEFT, padx=(2, 2))
 
-        self.alpha_var = tk.StringVar()
-        self.alpha_var.set('50.0')
-        alpha_entry = ttk.Entry(frame, width=10, textvariable=self.alpha_var)
+        self.alpha_var = tk.StringVar(value=str(self.default_alpha))
+        # self.alpha_var.set(str(self.default_alpha))
+        alpha_entry = ttk.Entry(
+            frame,
+            width=10,
+            textvariable=self.alpha_var
+        )
         alpha_entry.pack(side=tk.LEFT, padx=(2, 50))
 
     def create_menu(self):
@@ -73,75 +114,118 @@ class Window(ttk.Frame):
 
         file_menu = tk.Menu(menubar, tearoff=False)
         menubar.add_cascade(label='File', menu=file_menu)
-        file_menu.add_command(label='Open', command=self.open_file)
-        file_menu.add_command(label='Save', command=self.save_file)
+        file_menu.add_command(label='Open', command=self.open)
+        file_menu.add_command(label='Save', command=self.save)
 
     def click(self, event):
         scale = float(self.var_scale.get())
-        x = int(event.x / (1 + scale / 100))
-        y = int(event.y / (1 + scale / 100))
-        if x <= self.size[0] and y <= self.size[1]:
-            self.start_pt = (x, y)
-            print('click', self.start_pt)
+        self.scaled_size = Size(*self.size.scale(1 + scale / 100))
+
+        if self.scaled_size.is_inside(event):
+            self.start_pt = Point(event.x, event.y)
+
+            self.canvas.create_rectangle(
+                self.start_pt.x,
+                self.start_pt.y,
+                self.start_pt.x + 1,
+                self.start_pt.y + 1,
+                outline='#00FF00',
+                width=2,
+                tag='temp_rect'
+            )
+
+    def mouse_drag(self, event):
+        x, y = self.scaled_size.replace_outside_pt(event)
+        self.canvas.coords('temp_rect', self.start_pt.x, self.start_pt.y, x, y)
 
     def release(self, event):
         if self.start_pt:
-            scale = float(self.var_scale.get())
-            x = int(event.x / (1 + scale / 100))
-            y = int(event.y / (1 + scale / 100))
+            self.canvas.delete('temp_rect')
+            scale = self.scaled_size.rows / self.size.rows
+            pt0 = self.start_pt.get_original_pt(scale)
 
-            x = self.size[0] if y > self.size[0] else y
-            y = self.size[1] if x > self.size[1] else x
-            self.end_pt = (x, y)
-            print('release', self.end_pt)
-            self.draw_rectangle()
+            x, y = self.scaled_size.replace_outside_pt(event)
+            end_pt = Point(x, y)
+            pt1 = end_pt.get_original_pt(scale)
+
+            x0, x1 = min(pt0.x, pt1.x), max(pt0.x, pt1.x)
+            y0, y1 = min(pt0.y, pt1.y), max(pt0.y, pt1.y)
+
+            self.img_rgb[y0: y1 + 1, x0: x1 + 1] = [0, 255, 0]
+            # self.img_rgb[self.start_pt[1]: self.end_pt[1] + 1, self.start_pt[0]: self.end_pt[0] + 1] = [0, 255, 0]
+            # ndarrayのshape(行, 列)は、座標x, yと逆。
+            self.img_pil = Image.fromarray(self.img_rgb)
+            self.resize_img(self.var_scale.get())
 
         self.start_pt = None
 
     def draw_rectangle(self):
-        # self.img_rgb[self.start_pt[0]: self.end_pt[0] + 1, self.start_pt[1]: self.end_pt[1] + 1] = [255, 0, 0]
-        self.img_rgb[self.start_pt[1]: self.end_pt[1] + 1, self.start_pt[0]: self.end_pt[0] + 1] = [255, 0, 0]
+        self.img_rgb[self.start_pt[1]: self.end_pt[1] + 1, self.start_pt[0]: self.end_pt[0] + 1] = [0, 255, 0]
         # ndarrayのshape(行, 列)は、座標x, yと逆。
         self.img_pil = Image.fromarray(self.img_rgb)
         self.resize_img(self.var_scale.get())
         # arr[self.start_pt.x: self.end_pt.x + 1, self.start_pt.y: self/]
 
-    def open_file(self):
+    def open(self):
         file_type = [('image file', '*.png;*.jpg')]
         init_dir = pathlib.Path(__file__).parent
 
-        if file_path := tk.filedialog.askopenfilename(
+        if file_path := filedialog.askopenfilename(
                 filetypes=file_type, initialdir=init_dir):
             print(file_path)
             self.show_image(file_path)
 
-    def save_file(self):
+    def save(self):
         file_type = [('image file', '*.png;*.jpg')]
         init_dir = pathlib.Path(__file__).parent
 
-        if file_path := tk.filedialog.asksaveasfilename(
+        if file_path := filedialog.asksaveasfilename(
                 filetypes=file_type, initialdir=init_dir):
             print(file_path)
             self.save_image(file_path)
 
     def show_image(self, file_path):
-        img_bgr = cv2.imread(file_path)
-        self.img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-        self.size = self.img_rgb.shape
+        self.img_bgr = cv2.imread(file_path)
+        self.size = Size(*self.img_bgr.shape)
+        print(self.size)
+        # import pdb; pdb.set_trace()
+        # code = cv2.COLOR_BGRA2RGBA if self.size.color == 4 else cv2.COLOR_BGR2RGB
+        # self.img_rgb = cv2.cvtColor(self.img_bgr, code)
+        self.img_rgb = cv2.cvtColor(self.img_bgr, cv2.COLOR_BGR2RGB)
+        # self.size = Size(*self.img_rgb.shape)
         self.img_pil = Image.fromarray(self.img_rgb)
-        # self.img_pil = Image.open(file_path)
-        self.size = self.img_pil.size
         self.img_tk = ImageTk.PhotoImage(self.img_pil)
         self.canvas_id = self.canvas.create_image(0, 0, image=self.img_tk, anchor=tk.NW)
 
+    def validate_alpha(self):
+        try:
+            alpha = int(self.alpha_var.get())
+            return min(alpha, 255)
+        except ValueError:
+            messagebox.showwarning(
+                "Alert", "Enter a positive integer to alpha field.")
+            return None
+
     def save_image(self, file_path):
-        pass
+        if (alpha := self.validate_alpha()) is not None:
+
+            if self.size.color == 3:
+                self.img_bgr = np.insert(self.img_bgr, 3, 255, axis=2)
+
+            for i in range(self.size[1]):
+                for j in range(self.size[0]):
+                    arr = self.img_rgb[i, j]
+                    if arr[0] == 0 and arr[1] == 255 and arr[2] == 0:
+                        self.img_bgr[i, j][3] = alpha
+
+            cv2.imwrite(file_path, self.img_bgr)
 
     def resize_img(self, str_scale):
         scale = float(str_scale)
-        x = int((1 + scale / 100) * self.size[0])
-        y = int((1 + scale / 100) * self.size[1])
-        img_resize = self.img_pil.resize((x, y))
+        rows, cols = self.size.scale(1 + scale / 100)
+        # x = int((1 + scale / 100) * self.size[0])
+        # y = int((1 + scale / 100) * self.size[1])
+        img_resize = self.img_pil.resize((cols, rows))
         img_tk = ImageTk.PhotoImage(img_resize)
 
         self.canvas.itemconfig(self.canvas_id, image=img_tk)
